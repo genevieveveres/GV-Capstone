b7b146bcb3280f6c6cd851c393ecb863e110adc8
Chris Covey <chris.covey@tlgcohort.com>
2023-11-22 16:30:50 -0500

    Added colorized splash screen, AB#14

---
+++ src/star.odyssey/game/DisplaySplash.java
@@ -0,0 +1,49 @@
+package star.odyssey.game;
+
+import java.io.*;
+
+public class DisplaySplash {
+    // Displays text input in red
+
+    // clear screen
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+    public static String makeRed(String txt) {
+        return "\u001B[31m" + txt + "\u001B[0m";
+    }
+
+    // Displays text input in blue
+    public static String makeBlue(String txt) {
+        return "\u001B[34m" + txt + "\u001B[0m";
+    }
+
+    // Display splash screen
+    public static void displaySplash(String file) throws IOException {
+        clearScreen();
+        try {
+            FileReader fileReader = new FileReader(file);
+
+            int i;
+            while ((i = fileReader.read()) != -1) {
+                if ((char) i == '█') {
+                    System.out.print(makeRed(String.valueOf((char) i)));
+                } else if ((char) i == '░') {
+                    System.out.print(makeBlue(String.valueOf((char) i)));
+                } else {
+                    System.out.print((char) i);
+                }
+            }
+        }
+        catch (Exception e) {
+            // Handle exceptions (e.g., file not found, JSON parsing error)
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String file = "./data/splashScreen.txt";
+        displaySplash(file);
+    }
+}
