f1cbba4b73a06ab1597964c1eca5f7a8f93c64bd
Angel Villasante <Angel.VillasanteAmaran@tlgcohort.com>
2023-12-07 09:57:47 -0800

    InputBox that accepts text, AB#558
    - Added an input box, this one will accept text when Enter is pressed then clear itself, this text will be displayed in the existing label.

--- src/star/odyssey/ui/swing/MainFrame.java
+++ src/star/odyssey/ui/swing/MainFrame.java
@@ -1,11 +1,16 @@
 package star.odyssey.ui.swing;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MainFrame extends JFrame {
     private JPanel mainPanel;
     private JButton clickMeButton;
     private JLabel label1;
+    private JTextField textField1;
 
     public MainFrame(){
         this.setContentPane(mainPanel);
@@ -14,5 +19,13 @@ public class MainFrame extends JFrame {
         this.setLocationRelativeTo(null);
         this.setVisible(true);
         this.setResizable(false);
+
+        clickMeButton.addActionListener(this::clickMeButton_Click);
+        textField1.addActionListener(this::clickMeButton_Click);
+    }
+
+    private void clickMeButton_Click(ActionEvent e){
+        label1.setText(textField1.getText());
+        textField1.setText("");
     }
 }
